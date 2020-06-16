3847b43aa0699a2854d14b13732b269494204a0b
rsr986 <rsr986@gmail.com>
2020-06-16 10:29:25 +0530

    Initial commit

---
+++ 2-exercise-files/before/FirstTest/DemoWebApp.Tests/LoanApplicationTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DemoWebApp.Tests
+{
+    class LoanApplicationTests
+    {
+        [Fact]
+        public void StartApplication()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+    }
+}
